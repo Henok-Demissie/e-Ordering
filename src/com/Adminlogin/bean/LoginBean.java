9ab83fc1ad5b3863b372bceca498bbba242b7933
Henok-Demissie <101617532+Henok-Demissie@users.noreply.github.com>
2025-01-11 17:42:18 +0400

    Update LoginBean.java

--- src/com/Adminlogin/bean/LoginBean.java
+++ src/com/Adminlogin/bean/LoginBean.java
@@ -2,27 +2,57 @@ package com.Adminlogin.bean;
 
 import java.io.Serializable;
 
+/**
+ * A bean class to encapsulate login credentials.
+ * Implements {@link Serializable} to allow easy serialization for session management.
+ */
 public class LoginBean implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private String username;
-	private String password;
+    private static final long serialVersionUID = 1L;
 
-	public String getUsername() {
-		return username;
-	}
+    private String username;
+    private String password;
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    /**
+     * Gets the username.
+     * 
+     * @return the username as a {@link String}.
+     */
+    public String getUsername() {
+        return username;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    /**
+     * Sets the username.
+     * 
+     * @param username the username to set.
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    /**
+     * Gets the password.
+     * 
+     * @return the password as a {@link String}.
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Sets the password.
+     * 
+     * @param password the password to set.
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginBean{" +
+                "username='" + username + '\'' +
+                ", password='******'" +  // Hides the actual password for security purposes
+                '}';
+    }
 }

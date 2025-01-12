776dce9145a4575f0fa321aabfa8076704145e77
Henok-Demissie <101617532+Henok-Demissie@users.noreply.github.com>
2025-01-12 13:28:36 +0400

    Update LoginServlet.java

--- src/com/customer/LoginServlet.java
+++ src/com/customer/LoginServlet.java
@@ -11,35 +11,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		PrintWriter out = response.getWriter();
-		response.setContentType("text/html");
-		
-		String userName = request.getParameter("username");
-		String password = request.getParameter("password");
-		boolean isTrue;
-		
-		isTrue = CustomerDBUtil.validate(userName, password);
-		
-		if (isTrue == true) {
-			List<Customer> cusDetails = CustomerDBUtil.getCustomer(userName);
-			request.setAttribute("cusDetails", cusDetails);
-			
-			RequestDispatcher dis = request.getRequestDispatcher("useraccount.jsp");
-			dis.forward(request, response);
-		} else {
-			out.println("<script type='text/javascript'>");
-			out.println("alert('Your username or password is incorrect');");
-			out.println("location='profilelogin.jsp'");
-			out.println("</script>");
-		}
-		
-	}
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Set up response output
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
+        // Retrieve user credentials from the form
+        String userName = request.getParameter("username");
+        String password = request.getParameter("password");
+        boolean isValidUser = CustomerDBUtil.validate(userName, password);
+
+        if (isValidUser) {
+            // If login is successful, retrieve the customer details and forward to user account page
+            List<Customer> cusDetails = CustomerDBUtil.getCustomer(userName);
+            request.setAttribute("cusDetails", cusDetails);
+
+            // Forward the request to the user account page
+            RequestDispatcher dispatcher = request.getRequestDispatcher("useraccount.jsp");
+            dispatcher.forward(request, response);
+        } else {
+            // If login fails, display an error message and redirect to login page
+            showErrorMessage(out, "Your username or password is incorrect", "profilelogin.jsp");
+        }
+    }
+
+    /**
+     * Helper method to display an error message and redirect to a specified page.
+     */
+    private void showErrorMessage(PrintWriter out, String message, String redirectPage) {
+        out.println("<script type='text/javascript'>");
+        out.println("alert('" + message + "');");
+        out.println("location='" + redirectPage + "';");
+        out.println("</script>");
+    }
 }
